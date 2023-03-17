987897903d891df92dcd5a8999e74e23fc4b5144
Sam <samrapley@protonmail.com>
2023-03-17 08:35:46 +0000

    first commit

---
+++ pages/testing.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+
+
+browser.get("http://techstepacademy.com/iframe-training")
+
+
+
+
+
+
+
+
+
+
+
